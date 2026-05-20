86dd76b6c74449b952f03d285800c67f575832d4
Greg Knowles <Gknowles37@gmail.com>
2026-05-20 11:16:32 +0100

    Refactor finetune.sh to use CHECKPOINT_DIR variable

--- mobileposer/finetune.sh
+++ mobileposer/finetune.sh
@@ -1,15 +1,17 @@
-#!/bin/bash
+  #!/bin/bash
 
-if [ "$1" == "dip" ]; then
-    echo "Finetuning on DIP..." 
-    [ -d "checkpoints/$2/finetuned_dip" ] && rm -r "checkpoints/$2/finetuned_dip"
-    python train.py --module joints --init-from checkpoints/$2/joints --finetune dip
-    python train.py --module poser --init-from checkpoints/$2/poser --finetune dip
-elif [ "$1" == "imuposer" ]; then 
-    echo "Finetuning on IMUPoser..." 
-    [ -d "checkpoints/$2/finetuned_imuposer" ] && rm -r "checkpoints/$2/finetuned_imuposer"
-    python train.py --module joints --init-from checkpoints/$2/finetuned_dip/joints --finetune imuposer
-    python train.py --module poser --init-from checkpoints/$2/finetuned_dip/poser --finetune imuposer
-else
-    echo "Invalid argument. Please specify 'dip' or 'imuposer'"
-fi
+  CHECKPOINT_DIR="/app/MobilePoser/checkpoints"
+
+  if [ "$1" == "dip" ]; then
+      echo "Finetuning on DIP..."
+      [ -d "$CHECKPOINT_DIR/$2/finetuned_dip" ] && rm -r "$CHECKPOINT_DIR/$2/finetuned_dip"
+      python train.py --module joints --init-from $CHECKPOINT_DIR/$2/joints --finetune dip
+      python train.py --module poser --init-from $CHECKPOINT_DIR/$2/poser --finetune dip
+  elif [ "$1" == "imuposer" ]; then
+      echo "Finetuning on IMUPoser..."
+      [ -d "$CHECKPOINT_DIR/$2/finetuned_imuposer" ] && rm -r "$CHECKPOINT_DIR/$2/finetuned_imuposer"
+      python train.py --module joints --init-from $CHECKPOINT_DIR/$2/finetuned_dip/joints --finetune imuposer
+      python train.py --module poser --init-from $CHECKPOINT_DIR/$2/finetuned_dip/poser --finetune imuposer
+  else
+      echo "Invalid argument. Please specify 'dip' or 'imuposer'"
+  fi
